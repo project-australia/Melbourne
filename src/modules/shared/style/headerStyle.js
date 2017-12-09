aed4e279ba5943c722a4957d61eaec562177d6c3
Hebert Porto <hebertporto@gmail.com>
2017-12-09 06:53:27 -0200

    feat: ajustando as cores

--- src/modules/shared/style/headerStyle.js
+++ src/modules/shared/style/headerStyle.js
@@ -1,7 +1,7 @@
 export const navBar = {
   paddingLeft: '8.33333333%',
   paddingRight: '8.33333333%',
-  backgroundColor: 'rgb(229, 61, 61)',
+  backgroundColor: '#FF5746',
   color: 'rgba(0, 0, 0, 0.87)',
   transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
   boxSizing: 'border-box',
