033c291c24efb14b7ffdb3446a1857567bb51b41
Hebert Porto <hebertporto@gmail.com>
2018-03-21 22:05:35 -0300

    mais fields do user

--- src/modules/admin/components/UserEdit.jsx
+++ src/modules/admin/components/UserEdit.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Modal, Button, Input, Label } from 'semantic-ui-react'
+import { Modal, Button, Input, Dropdown } from 'semantic-ui-react'
 
 import './style/UserEdit.css'
 
 class UserEdit extends Component {
+  state = {
+    role: '',
+    name: '',
+    email: '',
+    birthDate: '',
+    telephone: '',
+    school: '',
+    wallet: {
+      ballance: ''
+    },
+    paypalAccount: ''
+  }
+  handleRoleChange = (e, { value }) => this.setState({ role: value })
+  handleClubChange = (e, { value }) => this.setState({ clue: value })
+  handleInputChange = (e) => this.setState({ [e.target.name]: e.target.value })
+  handleInputWalletChange = (e) => this.setState({ wallet: {ballance: e.target.value} })
+  clubOptions = () => [
+    { key: 'NONE', value: 'NONE', text: 'NONE' },
+    { key: 'TWENTY', value: 'TWENTY', text: 'ELITE CLUB' }
+  ]
+  roleOptions = () => [
+    { key: 'ADMIN', value: 'ADMIN', text: 'ADMIN' },
+    { key: 'REP', value: 'REP', text: 'REP' },
+    { key: 'USER', value: 'USER', text: 'USER' }
+  ]
   render () {
     return (
       <Modal
@@ -19,10 +44,121 @@ class UserEdit extends Component {
         <Modal.Content>
           <div className="ue-body">
             <div className="ue-item">
-              <Label size="big">
+              <span className="ue-label">
+                Role
+              </span>
+              <span className="ue-info">
+                <Dropdown
+                  placeholder="Select Role"
+                  onChange={this.handleRoleChange}
+                  selection
+                  value={this.state.role}
+                  options={this.roleOptions()} />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
                 Name
-              </Label>
-              <Input fluid placeholder='User Level...' />
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='name'
+                  disabled
+                  fluid
+                  placeholder='Name' />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Email
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='email'
+                  disabled
+                  fluid
+                  placeholder='Email' />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Birthdate
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='birthDate'
+                  disabled
+                  onChange={this.handleInputChange}
+                  fluid
+                  value={this.state.birthDate}
+                  placeholder='Birthdate' />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Telephone
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='telephone'
+                  onChange={this.handleInputChange}
+                  fluid
+                  value={this.state.telephone}
+                  placeholder='Telephone' />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Club
+              </span>
+              <span className="ue-info">
+                <Dropdown
+                  placeholder="Select Club"
+                  onChange={this.handleClubChange}
+                  selection
+                  value={this.state.club}
+                  options={this.clubOptions()} />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Wallet
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='wallet'
+                  onChange={this.handleInputWalletChange}
+                  fluid
+                  value={this.state.wallet.ballance}
+                  placeholder='Wallet Ballance' />
+              </span>
+            </div>
+          </div>
+          <div className="ue-body">
+            <div className="ue-item">
+              <span className="ue-label">
+                Paypal Account
+              </span>
+              <span className="ue-info">
+                <Input
+                  name='paypalAccount'
+                  onChange={this.handleInputChange}
+                  fluid
+                  value={this.state.paypalAccount}
+                  placeholder='Paypal Account' />
+              </span>
             </div>
           </div>
         </Modal.Content>
